67ce73933226ba4c466f436e7fbdede9a696ff4a
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-08-03 05:48:32 +0530

    Update atoms.jsx

--- atoms.jsx
+++ atoms.jsx
@@ -1,3 +1,4 @@
+/*
 import { atom, selector } from "recoil";
 
 //defing the atoms / states
@@ -32,5 +33,31 @@ export const totalNotificationSelector = selector({
         const messagingAtomCount = get(messagingAtom);
         //and then doing the operation .. we do or specify the operation in return inside a selector 
         return networkAtomCount + jobsAtomCount + notificationAtomCount + messagingAtomCount;
+    }
+})
+*/
+
+import axios from "axios";
+import { atom, selector } from "recoil";
+
+export const notifications = atom({
+    key: "networkAtom",
+    default: selector({
+        key : "networkAtomsSelector",
+        get : async () => {
+            const res = await axios.get("https://sum-server.100xdevs.com/notifications");
+            return res.data;
+        }
+    })
+});
+
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get: ({get}) => {
+        const allNotifications = get(notifications);
+        return allNotifications.network + 
+        allNotifications.jobs + 
+        allNotifications.notifications + 
+        allNotifications.messaging
     }
 })
