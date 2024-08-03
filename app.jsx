d3a9a3bd99223611c54b4bee5d50d50e2d923b7b
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-08-03 05:47:59 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from 'react'
 import './App.css'
 import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
@@ -34,6 +35,55 @@ function MainApp() {
        <button>Me ({totalNotificationCount})</button>
        
     </div>
+  )
+}
+
+export default App
+*/
+
+//same thing more advancely >> or moreover using asynchronous data queries ...mainly initialising 
+
+
+import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { notifications, totalNotificationSelector } from './atoms'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+//asynchronous queries is nothing but the values are coming from the backened and we are directly showing the value from the backend the link  to backend is an already created backend server
+//The default value cant be async the default value can be a selecto by itself
+
+function App() {
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+}
+
+function MainApp() {
+  const [networkCount, setNetworkCount] = useRecoilState(notifications); //notifications is the atom and when we fetch the data from the backend and updatte the state by setnetworkcount it basically updates the notifications state directly
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector); //selector
+
+  /* we can do this logic directly inside the atom in the atom.js file otherwise we will see a flash that is 0 in the notifications then it will show the otification counts see the atoms.js file 
+  useEffect(() => {
+    // fetch
+    axios.get("https://sum-server.100xdevs.com/notifications")
+      .then(res => {
+        setNetworkCount(res.data)
+      })
+  }, [])
+  */
+
+  return (
+    <>
+      <button>Home</button>
+      
+      <button>My network ({networkCount.networks >= 100 ? "99+" : networkCount.networks})</button>
+      <button>Jobs {networkCount.jobs}</button>
+      <button>Messaging ({networkCount.messaging})</button>
+      <button>Notifications ({networkCount.notifications})</button>
+
+      <button>Me ({totalNotificationCount})</button>
+    </>
   )
 }
 
